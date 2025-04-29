95dbbd888822f24a7f732f29b8f666432578857d
Aleksandra <escovaaleksandra@gmail.com>
2025-04-29 13:50:30 +0200

    TaskSolution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,8 +3,25 @@
  *
  * @return {number}
  */
+
 function calculateRentalCost(days) {
-  // write code here
+  const MIDL_TERM = 3;
+  const MIDL_TERM_DISCOUNT = 20;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const basePrice = days * 40;
+
+  if (days > 0 && days < MIDL_TERM) {
+    return basePrice;
+  }
+
+  if (days >= MIDL_TERM && days < LONG_TERM) {
+    return basePrice - MIDL_TERM_DISCOUNT;
+  }
+
+  if (days >= LONG_TERM) {
+    return basePrice - LONG_TERM_DISCOUNT;
+  }
 }
 
 module.exports = calculateRentalCost;
